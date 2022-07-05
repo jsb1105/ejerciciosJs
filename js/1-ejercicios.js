64bab93f93f628684c08ec93bd235347deb22e5c
JSebastian <jsebastianbarrios11@gmail.com>
2022-07-05 00:50:14 -0300

    primera parte de ejercicios

---
+++ js/1-ejercicios.js
@@ -0,0 +1,15 @@
+//1- Programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+alert("Un mensaje");
+
+// 2- Programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+document.write("<p>Hello World</p>");
+
+//3- Programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+document.write("Resultado"+(3+5));
+
+//4- Programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+let nombre="Sebastian Barrios"
+prompt("Nombre del usuario");
+document.write("<p>Hola:" + nombre + "</p>");
+
+//5-Programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
